07ac0e44bf5136760e382d3f3bb52c630974d7b3
Steve <stevedsimkins@gmail.com>
2023-09-16 21:56:47 -0400

    install script rewrite

--- install.sh
+++ install.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Define the name of the binary and the URL to download it from
-BINARY_NAME="pinata-go-cli"
-BINARY_URL="https://github.com/stevedylandev/pinata-go-cli/pinata-go-cli"
+cp ./pinata-go-cli /usr/local/bin/
 
-# Define the installation directory (where you want to install the binary)
-INSTALL_DIR="/usr/local/bin"
-
-# Download the binary
-echo "Downloading $BINARY_NAME..."
-wget -O "$INSTALL_DIR/$BINARY_NAME" "$BINARY_URL"
-
-# Check if the download was successful
-if [ $? -eq 0 ]; then
-	echo "Download complete."
-	chmod +x "$INSTALL_DIR/$BINARY_NAME" # Make the binary executable
-	echo "Setting execute permissions..."
-else
-	echo "Download failed. Installation aborted."
-	exit 1
-fi
-
-# Print a success message
-echo "Installation complete. You can now use $BINARY_NAME."
+echo "Install complete"
