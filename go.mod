d17f5e3f83e8154f9692479cd9a95806f8bf132d
Steve <stevedsimkins@gmail.com>
2024-01-11 14:33:59 -0500

    nvm

--- go.mod
+++ go.mod
@@ -17,5 +17,3 @@ require (
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/term v0.12.0 // indirect
 )
-
-replace github.com/stevedylandev/pinata-go-cli@latest => pinata
--- main.go
+++ main.go
@@ -59,6 +59,7 @@ type RequestsResponse struct {
 }
 
 func main() {
+  exeName := "pinata"
 	app := &cli.App{
 		Name:  "pinata",
 		Usage: "A CLI for uploading files to Pinata! To get started make an API key at https://app.pinata.cloud/keys, then authorize the CLI with the auth command with your JWT",
